5335cabfb526e49d9ab17670686590901ef09b43
Greg Soertsz <gsoertsz-transurbantech@transurban.tech>
2018-04-20 11:33:11 +1000

    initial commit incorporating basic hello world service

---
+++ sb-actuator-service/src/main/java/hello/HelloWorldController.java
@@ -0,0 +1,23 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/hello-world/{name}")
+    @ResponseBody
+    public Greeting sayHello(@PathVariable("name") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
